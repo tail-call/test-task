5b34dffa76f531d5a1d5c9f4866a09d2ed5d4324
T. C <infernalsaurian@gmail.com>
2018-07-06 22:16:01 +0700

    Bogus site list

--- reactpopup/src/App.js
+++ reactpopup/src/App.js
@@ -3,19 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            // TODO: remove these
+            sites: [
+	        {
+		    "name": "yandex",
+		    "domain": "yandex.ru",
+		    "message": "Hello %username%! My name is Yandex!"
+	        },
+	        {
+		    "name": "google",
+		    "domain": "google.ru",
+		    "message": "Hello %username%! My name is Google!"
+	        },
+	        {
+		    "name": "bing",
+		    "domain": "bing.com",
+		    "message": "Hello %username%! My name is Bing!"
+	        }
+            ]
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+              <h1>Sites list</h1>
+              <ul>
+                {
+                    this.state.sites
+                        .map(site => <li>{site.name}</li>)
+                }
+              </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
