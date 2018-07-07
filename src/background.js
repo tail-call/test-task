d9a5502e8cf090ecf5656b37994ef7bd92d531d7
T. C <infernalsaurian@gmail.com>
2018-07-07 13:56:10 +0700

    Now site list is updated once every hour

--- src/background.js
+++ src/background.js
@@ -3,6 +3,8 @@
 "use strict";
 
 const SITE_LIST_URL = "http://www.softomate.net/ext/employees/list.json";
+// Один час в миллисекундах
+const ONE_HOUR = 3600 * 1000;
 
 // Функции-обёртки для chrome.storage на промисах
 
@@ -20,24 +22,41 @@ function chromeGet(key) {
 
 function refreshSitesFrom(url) {
     return fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            // Сохранить временную метку
+            chromeSet("lastRefreshed", Date.now());
+            return response.json();
+        })
         .then(sites => {
             return chromeSet("sites", sites);
         });
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(sender.tab
-                ? "from a content script:" + sender.tab.url
-                : "from the extension");
+function scheduleRefresh(url) {
+    return chromeGet("lastRefreshed")
+        .then(({ lastRefreshed }) => {
+            // Предполагать, что время последнего обновления равно
+            // при первом запуске
+            console.log("lastRefreshed = " + lastRefreshed);
+            let delta = Date.now() - (lastRefreshed || 0);
+            if (delta >= ONE_HOUR) {
+                console.log("refreshing site list");
+                refreshSitesFrom(url);
+            }
+            // Повторить вызов через delta миллисекунд
+            setTimeout(scheduleRefresh, delta);
+        });
+}
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "site_list") {
         chromeGet("sites").then(sendResponse);
     }
-
-    // Чтобы sendResponse() мог работать асинхронно, нужно возвратить true.
+    // Чтобы sendResponse() мог работать асинхронно, нужно возвратить true:
     // <https://developer.chrome.com/extensions/messaging#simple>
     return true;
 });
 
-refreshSitesFrom(SITE_LIST_URL);
+
+// При старте расширения запустить цикл обновления
+scheduleRefresh(SITE_LIST_URL);
