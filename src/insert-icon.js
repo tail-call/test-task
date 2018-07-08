987c684c655f3fd2ab8a343d7587ad27e7474e02
T. C <infernalsaurian@gmail.com>
2018-07-08 16:35:02 +0700

    insert-icon.js: simplify a little bit

--- src/insert-icon.js
+++ src/insert-icon.js
@@ -9,40 +9,26 @@ function getHostFromURL(url) {
     return withoutWWW((new URL(url)).hostname);
 }
 
-// Эвристические функции для поиска места вставки иконки. Каждая
-// возвращает массив элементов, перед которыми должна быть вставлена
-// иконка.
-const heuristics = {
-    "google": hosts => {
-        // Каждый результат поиска обрамлён элементом с классом .g
-        return Array.from(document.querySelectorAll(".g"))
-        // Извлекаем первый элемент <a> из каждого
-            .map(el => el.getElementsByTagName("a")[0])
-        // По непонятным причинам в выдаче могут оказаться ссылки с
-        // href="mailto:...". Оставить только href="http..."
-            .filter(a => a.href.startsWith("http"))
-        // Оставить только ссылки, ссылающиеся на сайт из hosts
-            .filter(a => hosts.includes(getHostFromURL(a.href)));
-    },
-    "bing": hosts => {
-        // Каждый результат поиска обрамлён элементом с классом
-        // .b_algo
-        return Array.from(document.querySelectorAll(".b_algo"))
-        // Извлекаем первый элемент <a> из каждого
-            .map(el => el.getElementsByTagName("a")[0])
-        // Оставить только ссылки, ссылающиеся на сайт из hosts
-            .filter(a => hosts.includes(getHostFromURL(a.href)));
-    }
-};
-
-// Выбирает эвристическую функцию для сайта с именем хоста host
-function getHeuristicFor(host) {
-    if (host.match("bing")) {
-        return heuristics.bing;
-    } else {
-        return heuristics.google;
-    }
+// Эвристический алгоритм для нахождения ссылок в результатах
+// поиска. Параметр wrapperClass — имя класса, присвоенного элементам
+// с результатами поиска
+function findSearchResults(hosts, wrapperClass) {
+    return Array.from(document.querySelectorAll(wrapperClass))
+    // Извлекаем первый элемент <a> из каждого
+        .map(el => el.getElementsByTagName("a")[0])
+    // Оставить только href="http..."
+        .filter(a => a.href.startsWith("http"))
+    // Оставить только ссылки, ссылающиеся на сайт из hosts
+        .filter(a => hosts.includes(getHostFromURL(a.href)));
 }
+
+// Имена классов элементов с результатами поиска на разных поисковых
+// сайтах
+const wrapperClasses = {
+    "google.com": ".g",
+    "google.ru": ".g",
+    "bing.com": ".b_algo"
+};
 
 function makeIcon() {
     let icon = document.createElement("img");
@@ -53,8 +39,10 @@ function makeIcon() {
 // Вставляет иконки рядом с ссылками на сайты из массива hosts
 // среди результатов поиска
 function insertIcons(hosts) {
-    let heuristic = getHeuristicFor(document.location.host);
-    let nodes = heuristic(hosts);
+    let nodes = findSearchResults(
+        hosts,
+        wrapperClasses[withoutWWW(document.location.host)]
+    );
     for (let node of nodes) {
         node.parentElement.insertBefore(makeIcon(), node);
     }
